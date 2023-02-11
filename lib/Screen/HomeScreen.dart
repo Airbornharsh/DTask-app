eb92c98c8658b95e06badf4574fe7456eda528db
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-02-11 23:56:01 +0530

    Changed the HomePage View

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -18,6 +18,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // int dropdownindex = 0;
+
   @override
   Widget build(BuildContext context) {
     if (widget.start) {
@@ -29,6 +31,12 @@ class _HomeScreenState extends State<HomeScreen> {
         widget.start = false;
       });
     }
+
+    final items = [
+      'Filter',
+      'Days',
+      'Month',
+    ];
 
     Widget getWidgetAccordingDrawer() {
       switch (Provider.of<Settings>(context).getSelectedDrawerIndex) {
@@ -42,17 +50,17 @@ class _HomeScreenState extends State<HomeScreen> {
           return const Months();
 
         default:
-          return Container();
+          return const Text("ok");
       }
     }
 
-    var appBarTitle = ["DTask", "Sorted By Days", "Sorted By Months"];
+    // var appBarTitle = ["DTask", "Sorted By Days", "Sorted By Months"];
 
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-        appBarTitle[Provider.of<Settings>(context).getSelectedDrawerIndex],
-      )),
+      // appBar: AppBar(
+      //     title: Text(
+      //   appBarTitle[Provider.of<Settings>(context).getSelectedDrawerIndex],
+      // )),
       floatingActionButton: ClipRRect(
         borderRadius: BorderRadius.circular(200),
         child: Container(
@@ -75,22 +83,50 @@ class _HomeScreenState extends State<HomeScreen> {
               tooltip: "Add",
             )),
       ),
-      drawer: const HomeDrawer(),
+      // drawer: const HomeDrawer(),
       body: Container(
         decoration: BoxDecoration(color: Colors.grey.shade200),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(
+              height: 8,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                "lib/assets/DTask_Logo.png",
+                height: 50,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 40,
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+              margin: const EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 252, 252, 252),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: DropdownButton(
+                icon: const Icon(Icons.filter_alt_rounded),
+                value: Provider.of<Settings>(context).getSelectedDrawerIndex,
+                items: [
+                  DropdownMenuItem(value: 0, child: Text(items[0])),
+                  DropdownMenuItem(value: 1, child: Text(items[1])),
+                  DropdownMenuItem(value: 2, child: Text(items[2]))
+                ],
+                onChanged: (value) {
+                  Provider.of<Settings>(context, listen: false)
+                      .setSelectedDrawerIndex(value as int);
+                },
+              ),
+            ),
             const SizedBox(
               height: 10,
             ),
-            // DragTarget(
-            //   onAccept: (data) {
-            //     print(data);
-            //   },
-            //   builder: (context, candidateData, rejectedData) {
-            //     return const Icon(Icons.delete);
-            //   },
-            // ),
             Expanded(
               child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 5),
